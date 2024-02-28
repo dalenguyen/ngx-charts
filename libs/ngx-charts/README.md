c8ad77a6235bcc12318752e4a4c7756f8eb95910
Dale Nguyen <dale.nguyen@noibu.com>
2024-02-28 16:14:22 -0500

    add init lib

---
+++ libs/ngx-charts/README.md
@@ -0,0 +1,7 @@
+# ngx-charts
+
+This library was generated with [Nx](https://nx.dev).
+
+## Running unit tests
+
+Run `nx test ngx-charts` to execute the unit tests.
---
+++ libs/ngx-charts/src/lib/ngx-charts.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'dngx',
+  standalone: true,
+  imports: [CommonModule],
+  template: `<p>ngx-charts works!</p>`,
+  styles: ``,
+})
+export class NgxChartsComponent {}
